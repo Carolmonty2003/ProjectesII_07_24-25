621b5b0d15b85184bbc08254a8158085e7405aa0
carolmonty <carolina.monteagudo@enti.cat>
2024-12-15 18:04:46 +0100

    refactor level wrath

--- Assets/Scripts/Platforms/DamageObjects.cs
+++ Assets/Scripts/Platforms/DamageObjects.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DamageObjects : MonoBehaviour
+public class DamageObjects : MonoBehaviour 
 {
     private void OnTriggerEnter2D(Collider2D  collision)
     {
