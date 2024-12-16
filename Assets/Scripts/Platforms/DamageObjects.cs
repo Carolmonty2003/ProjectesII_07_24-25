8d85f62fdb9aa7dd1ef28d49d62b840ccf8903a0
carolmonty <carolina.monteagudo@enti.cat>
2024-12-16 04:57:22 +0100

    Update some plataforms

--- Assets/Scripts/Platforms/DamageObjects.cs
+++ Assets/Scripts/Platforms/DamageObjects.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class DamageObjects : MonoBehaviour 
 {
-    private void OnTriggerEnter2D(Collider2D  collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.transform.CompareTag("Player")){
-            Debug.Log("Player Died");
-            collision.transform.GetComponent<PlayerRespawn>().PlayerDied();
+        if (collision.transform.CompareTag("Player"))
+        { 
+            if (!collision.gameObject.GetComponent<PlayerController>().EresMuyGrande())
+            {
+                Debug.Log("Player Died");
+                collision.transform.GetComponent<PlayerRespawn>().PlayerDied();
+            }
         }
     }
 
